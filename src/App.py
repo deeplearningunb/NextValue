da56476444251c99d8d6403b768d0a19adf7154d
Henrike100 <henrikem100@gmail.com>
2021-04-21 00:32:09 -0300

    Adição de mais telas

--- src/App.py
+++ src/App.py
@@ -4,17 +4,28 @@ from os.path import isfile, join
 from pages import (
     StartPage,
     ConfigurationPage,
-    CryptocurrencyChoicePage,
+    TrainingPage,
     ResultPage
 )
+from Layer import Layer
+import pandas as pd
+import numpy as np
+import math
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 
-DEFAULT_DAYS = 50
+DEFAULT_DAYS = 5
 DEFAULT_OPTIMIZER = "adam"
 DEFAULT_LOSS = "mean_squared_error"
-DEFAULT_EPOCHS = 50
-DEFAULT_BATCH_SIZE = 128
-UNITS = 600
-DROPOUT = 0.2
+DEFAULT_EPOCHS = 300
+DEFAULT_BATCH_SIZE = 96
+
+LAYERS = [
+    Layer(units=300, dropout=0.2)
+]
 
 DATA_PATH = "../Prices"
 
@@ -28,7 +39,8 @@ class App(tk.Tk):
             "loss": tk.StringVar(),
             "epochs": tk.StringVar(),
             "batch": tk.StringVar(),
-            "cryptocurrency": tk.StringVar(),
+            "layers": LAYERS,
+            "cryptocurrency": "Bitcoin",
             "cryptocurrency_list": [f for f in listdir(DATA_PATH) if (isfile(join(DATA_PATH, f)) and f.endswith(".csv"))],
         }
         self.shared_data["days"].set(DEFAULT_DAYS)
@@ -36,7 +48,10 @@ class App(tk.Tk):
         self.shared_data["loss"].set(DEFAULT_LOSS)
         self.shared_data["epochs"].set(DEFAULT_EPOCHS)
         self.shared_data["batch"].set(DEFAULT_BATCH_SIZE)
-        self.shared_data["cryptocurrency"].set("Selecione a criptomoeda")
+
+        self.NUMBER_OF_CRYPTOCURRENCIES = len(self.shared_data["cryptocurrency_list"])
+
+        self.process_data(self.shared_data["cryptocurrency_list"])
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
@@ -44,64 +59,115 @@ class App(tk.Tk):
         container.grid_columnconfigure(0, weight = 1)
 
         self.frames = {}
-        for F in (StartPage, ConfigurationPage, CryptocurrencyChoicePage, ResultPage):
+        for F in (StartPage, ConfigurationPage, TrainingPage, ResultPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky ="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(TrainingPage)
 
     def show_frame(self, c):
         frame = self.frames[c]
         frame.tkraise()
 
-    def process_data(self):
-        pass
+    def process_data(self, cryptocurrency_list):
+        cryptocurrency_list.sort()
+        TRAINING_RATE = 0.95
+        self.PREVIOUS_DAYS = int(self.shared_data["days"].get())
+        
+        self.dataset = pd.read_csv(DATA_PATH + "/Bitcoin.csv")
+        self.dataset = self.dataset.iloc[:, 1:3]
+
+        for i in range(0, self.NUMBER_OF_CRYPTOCURRENCIES):
+            file = pd.read_csv(DATA_PATH + "/" + cryptocurrency_list[i])
+            file = file.iloc[:, 1:3]
+            self.dataset = pd.merge(self.dataset, file, on='Date', how='outer')
+        
+        self.dataset = self.dataset.iloc[:, 2:self.NUMBER_OF_CRYPTOCURRENCIES+2]
+        self.dataset = self.dataset.replace(np.nan, 0)
+
+        NUMBER_OF_ROWS = self.dataset.shape[0]
+        self.TRAINING_SET_SIZE = math.ceil(NUMBER_OF_ROWS*TRAINING_RATE)
+        self.TEST_SET_SIZE = NUMBER_OF_ROWS - self.TRAINING_SET_SIZE
+
+        training_set = self.dataset.iloc[:self.TRAINING_SET_SIZE, : ].values
+        self.test_set = self.dataset.iloc[self.TRAINING_SET_SIZE:, : ].values
+
+        self.sc = MinMaxScaler(feature_range = (0, 1))
+        training_set_scaled = self.sc.fit_transform(training_set)
+
+        self.X_train = []
+        self.y_train = []
+        for i in range(self.PREVIOUS_DAYS, self.TRAINING_SET_SIZE):
+            self.X_train.append(training_set_scaled[i-self.PREVIOUS_DAYS:i, 0:self.NUMBER_OF_CRYPTOCURRENCIES])
+            self.y_train.append(training_set_scaled[i, 0:self.NUMBER_OF_CRYPTOCURRENCIES])
+
+        self.X_train, self.y_train = np.array(self.X_train), np.array(self.y_train)
+
+        self.X_train = self.X_train.reshape((self.X_train.shape[0], self.X_train.shape[1], self.NUMBER_OF_CRYPTOCURRENCIES))
 
     def build_rnn(self):
-        NUMBER_OF_LAYERS = len(self.layer_list)
-        NUMBER_OF_CRYPTOCURRENCIES = len(self.shared_data["cryptocurrency_list"])
+        layer_list = self.shared_data["layers"]
+        NUMBER_OF_LAYERS = len(layer_list)
 
         regressor = Sequential()
 
         if NUMBER_OF_LAYERS == 1:
-            layer = self.layer_list[0]
-            units = layer["units"]
-            dropout = layer["dropout"]
+            layer = layer_list[0]
+            units = layer.units
+            dropout = layer.dropout
 
-            regressor.add(LSTM(units = units, input_shape = (self.dataset.shape[1], NUMBER_OF_CRYPTOCURRENCIES)))
+            regressor.add(LSTM(units = units, input_shape = (self.X_train.shape[1], self.NUMBER_OF_CRYPTOCURRENCIES)))
 
             if dropout > 0:
                 regressor.add(Dropout(dropout))
         else:
-            first_layer = self.layer_list[0]
-            units = first_layer["units"]
-            dropout = first_layer["dropout"]
+            first_layer = layer_list[0]
+            units = first_layer.units
+            dropout = first_layer.dropout
 
-            regressor.add(LSTM(units = units, return_sequences = True, input_shape = (self.dataset.shape[1], NUMBER_OF_CRYPTOCURRENCIES)))
+            regressor.add(LSTM(units = units, return_sequences = True, input_shape = (self.dataset.shape[1], self.NUMBER_OF_CRYPTOCURRENCIES)))
 
             if dropout > 0:
                 regressor.add(Dropout(dropout))
 
             for i in range(1, NUMBER_OF_LAYERS-1):
-                layer = self.layer_list[i]
-                units = layer["units"]
-                dropout = layer["dropout"]
+                layer = layer_list[i]
+                units = layer.units
+                dropout = layer.dropout
 
                 regressor.add(LSTM(units = units, return_sequences = True))
 
                 if dropout > 0:
                     regressor.add(Dropout(dropout))
 
-            last_layer = self.layer_list[-1]
-            units = last_layer["units"]
-            dropout = last_layer["dropout"]
+            last_layer = layer_list[-1]
+            units = last_layer.units
+            dropout = last_layer.dropout
 
             regressor.add(LSTM(units = units))
 
             if dropout > 0:
                 regressor.add(Dropout(dropout))
 
+        regressor.add(Dense(units = self.NUMBER_OF_CRYPTOCURRENCIES))
+        regressor.compile(optimizer = self.shared_data["optimizer"].get(), loss = self.shared_data["loss"].get())
 
-        regressor.add(Dense(units = NUMBER_OF_CRYPTOCURRENCIES))
-        regressor.compile(optimizer = self.shared_data["optimizer"], loss = self.shared_data["loss"])
+        self.rnn = regressor
+    
+    def get_test_predict(self):
+        inputs = self.dataset[self.TRAINING_SET_SIZE-self.PREVIOUS_DAYS:].values
+        inputs = inputs.reshape((inputs.shape[0], inputs.shape[1]))
+        inputs = self.sc.transform(inputs)
+
+        self.X_test = []
+        for i in range(self.PREVIOUS_DAYS, self.PREVIOUS_DAYS+self.TEST_SET_SIZE):
+          self.X_test.append(inputs[i-self.PREVIOUS_DAYS:i, 0:self.NUMBER_OF_CRYPTOCURRENCIES])
+
+        self.X_test = np.array(self.X_test)
+        self.X_test = np.reshape(self.X_test, (self.X_test.shape[0], self.X_test.shape[1], self.NUMBER_OF_CRYPTOCURRENCIES))
+
+        predict = self.rnn.predict(self.X_test)
+        predict = self.sc.inverse_transform(predict)
+
+        return predict
--- src/pages.py
+++ src/pages.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 OTIMIZERS = [
     "adadelta",
@@ -70,7 +73,7 @@ class StartPage(tk.Frame):
             font = ("Verdana", 14),
             height = 2,
             width = 20,
-            command = lambda : controller.show_frame(CryptocurrencyChoicePage)
+            command = lambda : controller.show_frame(TrainingPage)
         )
         default_button.pack(pady=5)
 
@@ -79,7 +82,7 @@ class ConfigurationPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        self.alert = "Alerta"
+        self.alert = ""
 
         welcome_label = tk.Label(self, text ="Configurações Customizadas", font = ("Verdana", 14, "bold"))
         welcome_label.pack(pady=10)
@@ -154,48 +157,123 @@ class ConfigurationPage(tk.Frame):
             self.alert_label["text"] = "Deve ser um numero maior que 0"
             return
         
-        controller.show_frame(CryptocurrencyChoicePage)
+        controller.show_frame(TrainingPage)
 
 
-class CryptocurrencyChoicePage(tk.Frame):
+class TrainingPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        FONT = ("Verdana", 16)
+        self.cryptocurrency_list = [c[:-4] for c in controller.shared_data["cryptocurrency_list"]]
+        self.cryptocurrency_list.sort()
 
-        cryptocurrency_list = [c[:-4] for c in controller.shared_data["cryptocurrency_list"]]
-        cryptocurrency_list.sort()
+        self.test_set_size = controller.TEST_SET_SIZE
+        self.test_set = controller.test_set
 
-        welcome_label = tk.Label(self, text = "Ótimo! A rede neural está configurada!", font = ("Verdana", 35))
-        welcome_label.pack(fill=tk.X, pady=70)
+        welcome_label = tk.Label(self, text = "Clique no botão para começar a treinar", font = ("Verdana", 35))
+        welcome_label.pack(fill=tk.X, pady=30)
 
-        info_label = tk.Label(self, text = "Agora que as configurações foram definidas, falta uma coisa:", font = FONT)
-        info_label.pack(fill=tk.X, pady=5)
-        
-        training_label = tk.Label(self, text = "Você deseja prever o valor de qual criptomoeda?", font = FONT)
-        training_label.pack(fill=tk.X, pady=50)
+        self.progress = tk.Label(self, text ="", font = ("Verdana", 20))
+        self.progress.pack(side = tk.BOTTOM)
 
-        container_options = tk.Frame(self)
-        container_options.pack()
+        self.cryptocurrency = tk.StringVar()
+        self.cryptocurrency.set("Selecione a criptomoeda")
 
-        scrollbar = tk.Scrollbar(container_options)
-        scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
+        self.container_plot = tk.Frame(self, relief=tk.RAISED, height=500)
 
-        mylist = tk.Listbox(container_options, yscrollcommand = scrollbar.set, font = ("Verdana", 12))
-        for c in cryptocurrency_list:
-            mylist.insert(tk.END, c)
-        
-        mylist.pack()
-        scrollbar.config(command = mylist.yview)
+        self.cryptocurrency.trace("w", self.show_test)
 
-        train_button = tk.Button(
+        self.cryptocurrency_menu = tk.OptionMenu(self, self.cryptocurrency, *self.cryptocurrency_list)
+        self.cryptocurrency_menu.config(font = ("Verdana", 20))
+        self.cryptocurrency_menu["state"] = tk.DISABLED
+        self.cryptocurrency_menu.pack()
+
+        self.container_plot.pack()
+
+        config_button = tk.Button(
             self,
-            text = "Treinar Rede Neural",
+            text = "Treinar com outras\nconfigurações",
             font = ("Verdana", 14),
             height = 2,
             width = 20,
+            command = lambda : controller.show_frame(ConfigurationPage)
         )
-        train_button.pack(pady=80)
+        config_button.pack(side = tk.LEFT)
+
+        self.train_button = tk.Button(
+            self,
+            text = "Treinar",
+            font = ("Verdana", 14),
+            height = 2,
+            width = 20,
+            command = lambda : self.train(controller)
+        )
+        self.train_button.pack(side = tk.LEFT)
+
+        self.next_button = tk.Button(
+            self,
+            text = "Avançar",
+            font = ("Verdana", 14),
+            height = 2,
+            width = 20,
+            state = tk.DISABLED
+        )
+        self.next_button.pack(side = tk.LEFT)
+    
+    def train(self, controller):
+        self.train_button["state"] = tk.DISABLED
+        self.next_button["state"] = tk.DISABLED
+        self.cryptocurrency_menu["state"] = tk.DISABLED
+
+        controller.build_rnn()
+
+        total_epochs = int(controller.shared_data["epochs"].get())
+
+        X_train, y_train = controller.X_train, controller.y_train
+
+        batch_size = int(controller.shared_data["batch"].get())
+
+        for i in range(total_epochs):
+            controller.rnn.fit(X_train, y_train, epochs = 1, batch_size = batch_size, verbose=0)
+            controller.rnn.reset_states()
+            progress = ((i+1)*100) / total_epochs
+            self.progress["text"] = "{:.3f}".format(progress) + "%"
+            self.update_idletasks()
+        
+        self.train_button["state"] = tk.NORMAL
+        self.train_button["text"] = "Treinar\nnovamente"
+
+        self.next_button["state"] = tk.NORMAL
+        self.cryptocurrency_menu["state"] = tk.NORMAL
+
+        self.predict = controller.get_test_predict()
+
+        if(self.cryptocurrency.get() != "Selecione a criptomoeda"):
+            self.show_test()
+    
+    def show_test(self, *args):
+        option = self.cryptocurrency.get()
+        INDEX_OF_CRYPTOCURRENCY = self.cryptocurrency_list.index(option)
+        test_cryptocurrency = []
+        predict_cryptocurrency = []
+
+        for i in range(self.test_set_size):
+            test_cryptocurrency.append(self.test_set[i, INDEX_OF_CRYPTOCURRENCY])
+            predict_cryptocurrency.append(self.predict[i, INDEX_OF_CRYPTOCURRENCY])
+
+        for child in self.container_plot.winfo_children():
+            child.destroy()
+
+        fig = Figure(figsize=(7,5))
+        a = fig.add_subplot(111)
+        a.plot(test_cryptocurrency, color = 'red', label = 'Real ' + option + ' Price')
+        a.plot(predict_cryptocurrency, color = 'blue', label = 'Predicted ' + option + ' Price')
+        a.set_title(option + ' Price Prediction')
+        a.set_ylabel(option + ' Price', fontsize=14)
+        a.set_xlabel("Time", fontsize=14)
+        canvas = FigureCanvasTkAgg(fig, master=self.container_plot)
+        canvas.get_tk_widget().pack()
+        canvas.draw()
 
 
 class ResultPage(tk.Frame):
